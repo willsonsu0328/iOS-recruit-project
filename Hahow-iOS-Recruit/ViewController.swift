d871e204c6209d1bdf6e10a3cfc0e2ac94f2a6e7
Tommy Lin <tommy60703@gmail.com>
2021-10-05 15:06:48 +0800

    Add initial Xcode project

---
+++ Hahow-iOS-Recruit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hahow-iOS-Recruit
+//
+//  Created by Tommy Lin on 2021/10/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
