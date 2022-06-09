a8468270dae993ce283c5010b40d21e531565f18
wilson <wilson@linkwish.com>
2022-06-09 22:31:53 +0800

    add base view controllers

---
+++ Hahow-iOS-Recruit/Base/Controller/BaseViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BaseViewController.swift
+//  Hahow-iOS-Recruit
+//
+//  Created by Wilson on 2022/6/9.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .g_backgroundColor
+    }
+
+}
---
+++ Hahow-iOS-Recruit/Home/Controller/CourseHomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CourseHomeViewController.swift
+//  Hahow-iOS-Recruit
+//
+//  Created by Wilson on 2022/6/9.
+//
+
+import UIKit
+
+class CourseHomeViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
