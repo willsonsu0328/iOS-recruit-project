356005eb5524d486543c549b6a187ea1aa7fb883
wilson <wilson@linkwish.com>
2022-06-11 18:06:19 +0800

    add property category type in `CategoryModel`

--- Hahow-iOS-Recruit/Home/Model/CategoryModel.swift
+++ Hahow-iOS-Recruit/Home/Model/CategoryModel.swift
@@ -9,15 +9,42 @@ import UIKit
 
 struct CategoryModel: Codable {
 
-    private let _category: String?
-    private let _courses: [CourseModel]?
+    enum CategoryType: String, Codable {
+        case undefined = ""
+        case programming
+        case design
+        case language
+        case music
 
-    var category: String { _category ?? "" }
+        var title: String {
+
+            var title = ""
+            switch self {
+            case .programming:
+                title = NSLocalizedString("coursehome_categoryprogramming", comment: "")
+            case .design:
+                title = NSLocalizedString("coursehome_categorydesign", comment: "")
+            case .language:
+                title = NSLocalizedString("coursehome_categorylanguage", comment: "")
+            case .music:
+                title = NSLocalizedString("coursehome_categorymusic", comment: "")
+            case .undefined:
+                title = ""
+            }
+            return title
+        }
+    }
+
+    private let _courses: [CourseModel]?
+    private let _categoryType: CategoryType?
+
     var courses: [CourseModel] { _courses ?? [] }
+    var categoryType: CategoryType { _categoryType ?? .undefined}
+    var filterCourses: [CourseModel] = []
 
     enum CodingKeys: String, CodingKey {
-        case _category = "category"
         case _courses = "courses"
+        case _categoryType = "category"
     }
 
 }
